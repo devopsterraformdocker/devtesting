c97389903273bfa6bcf64223a8139122179b2ea0
devopsterraformdocker <devopsterraformdocker@gmail.com>
2025-08-22 10:08:02 +0530

    add Backend

--- main.tf
+++ main.tf
@@ -5,7 +5,14 @@ terraform {
       version = "4.22.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name = "Agrawest"
+    storage_account_name = "aug15"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "statefile"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "dev.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
 }
+
 
 provider "azurerm" {
     features {}
